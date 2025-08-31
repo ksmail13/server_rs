845931c456a5cbfbb7c3fef1b77f71f57821b6dd
ksmail13 <ksmail13@gmail.com>
2025-08-31 22:42:01 +0900

    fix server
    - add server
    - add echoprocessor for echo server
    - fix main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use clap::Parser;
 use server::ServerArgs;
 
-pub mod handler;
-pub mod server;
+use crate::server::Server;
+
+mod server;
+mod worker;
 
 fn main() {
-    let args = ServerArgs::parse();
+    colog::basic_builder()
+        .default_format()
+        .filter_level(log::LevelFilter::Trace)
+        .format_line_number(true)
+        .write_style(env_logger::fmt::WriteStyle::Always)
+        .init();
 
+    let args = ServerArgs::parse();
     println!("Open Server: {:?}", args);
+
+    let mut server = Server::new(args);
+    let _ = server.open_server().err();
 }
--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,14 @@
-use std::{net::{SocketAddrV4, TcpListener}};
+use std::{
+    io::{self, Read, Write},
+    net::{SocketAddr, TcpListener, TcpStream},
+    process::exit,
+    rc::Rc,
+    time::Duration,
+};
 
 use clap::Parser;
-use nix::libc::fork;
-use crate::handler::Worker;
+
+use crate::worker::{Worker, WorkerGroup, WorkerManager};
 
 const DEFAULT_HOST: &str = "0.0.0.0";
 
@@ -11,7 +17,7 @@ const DEFAULT_HOST: &str = "0.0.0.0";
 pub struct ServerArgs {
     #[arg(short, long, default_value_t = 10080)]
     port: u16,
-    #[arg(short, long, default_value_t = 10080)]
+    #[arg(short, long, default_value_t = 10079)]
     reserve_port: u16,
     #[arg(long, default_value_t = DEFAULT_HOST.to_string())]
     host: String,
@@ -19,55 +25,146 @@ pub struct ServerArgs {
     worker: u32,
     #[arg(short, long, default_value_t = 4)]
     reserve: u32,
+    #[arg(short, long, default_value_t = 500)]
+    timeout_ms: u64,
 }
 
 pub struct Server {
     config: ServerArgs,
-    main_handler: Box<dyn Worker>,
-    reserve_handler: Box<dyn Worker>,
+    main_manager: WorkerGroup,
+    reserve_manager: WorkerGroup,
 }
 
-impl std::ops::Deref for Server {
-    type Target = Box<dyn Worker>;
+pub struct TcpWorker {
+    timeout_ms: u64,
+    tcp_listener: TcpListener,
+    tcp_process: Box<dyn Process>,
+}
 
-    fn deref(&self) -> &Self::Target {
-        &self.main_handler
+pub trait Process {
+    fn process(&self, stream: TcpStream, client_addr: SocketAddr) -> io::Result<(usize, usize)>;
+    fn name(&self) -> String {
+        return "process".to_string();
+    }
+}
+
+#[derive(Debug)]
+pub struct EchoProcess {
+    prefix: Option<String>,
+}
+
+impl Process for EchoProcess {
+    fn process(&self, mut stream: TcpStream, client: SocketAddr) -> io::Result<(usize, usize)> {
+        let pid = nix::unistd::getpid();
+        let mut bufs: Vec<u8> = vec![0; 1024];
+        let mut all_readed = 0;
+        let mut all_writed = 0;
+        loop {
+            let read_result = stream.read(&mut bufs);
+
+            let echo_result = match read_result {
+                Ok(readed) => {
+                    if readed == 0 {
+                        break;
+                    }
+                    all_readed += readed;
+                    if let Some(prefix) = &self.prefix {
+                        let _ = stream.write_fmt(format_args!("{prefix}"));
+                    }
+                    stream.write(&bufs)
+                }
+                Err(ref read_err) => {
+                    log::error!(target: "MainWorker.process", "Read error: {read_err}");
+                    stream.write_fmt(format_args!("{read_err}")).map(|_| 0)
+                }
+            };
+
+            if echo_result.is_err() {
+                break;
+            }
+
+            all_writed += echo_result.unwrap();
+        }
+
+        log::info!(target:"access log", "{pid} {client} {all_readed} {all_writed}");
+
+        return Ok((all_readed, all_writed));
+    }
+
+    fn name(&self) -> String {
+        return "EchoProcess".to_string();
+    }
+}
+
+impl Worker for TcpWorker {
+    fn run(&self) {
+        let pid = nix::unistd::getpid();
+        let process = &self.tcp_process.name();
+        log::trace!(target: "TcpWorker.run", "TcpWorker start[{pid}:{process}]");
+        loop {
+            let stream_result = self.tcp_listener.accept();
+
+            match stream_result {
+                Ok((stream, client)) => {
+                    let _ = stream.set_write_timeout(Some(Duration::from_millis(self.timeout_ms)));
+                    let _ = self.tcp_process.process(stream, client);
+                }
+                Err(err) => {
+                    log::error!("Accept failed {err}");
+                    exit(1);
+                }
+            }
+        }
     }
 }
 
 impl Server {
-    pub fn open_server(&self) -> Result<i32, String> {
-        let main_addr: Result<SocketAddrV4, _> = format!("{}:{}", self.config.host, self.config.port).parse();
-        let reserve_addr: Result<SocketAddrV4, _> = format!("{}:{}", self.config.host, self.config.reserve_port).parse();
+    pub fn new(config: ServerArgs) -> Self {
+        let main_worker = config.worker;
+        let reserve_worker = config.reserve;
 
-        if let Err(err) = main_addr {
-            return Err(err.to_string());
-        }
+        return Self {
+            config,
+            main_manager: WorkerGroup::new(main_worker, None),
+            reserve_manager: WorkerGroup::new(reserve_worker, None),
+        };
+    }
 
-        if let Err(err) = reserve_addr {
-            return Err(err.to_string());
-        }
+    pub fn open_server(&mut self) -> Result<i32, String> {
+        loop {
+            let config = &self.config;
+            let main_connect =
+                TcpListener::bind(format!("{}:{}", config.host, config.port)).unwrap();
+            let reserve_connect = if self.config.reserve > 0 {
+                Some(TcpListener::bind(format!("{}:{}", config.host, config.reserve_port)).unwrap())
+            } else {
+                None
+            };
 
-        let main = TcpListener::bind(main_addr.unwrap());
-        let reserve = TcpListener::bind(reserve_addr.unwrap());
+            let mut group = vec![];
+            self.main_manager.set_worker(Some(Rc::new(TcpWorker {
+                timeout_ms: config.timeout_ms,
+                tcp_listener: main_connect,
+                tcp_process: Box::new(EchoProcess { prefix: None }),
+            })));
+            group.push(&mut self.main_manager);
 
-        let mut main_childs: Vec<i32> = vec![];
-        let mut reserve_childs: Vec<i32> = vec![];
+            if let Some(conn) = reserve_connect {
+                self.reserve_manager.set_worker(Some(Rc::new(TcpWorker {
+                    timeout_ms: config.timeout_ms,
+                    tcp_listener: conn,
+                    tcp_process: Box::new(EchoProcess {
+                        prefix: Some("reserve: ".to_string()),
+                    }),
+                })));
+                group.push(&mut self.reserve_manager);
+            }
 
-        for _ in 0..self.config.worker {
-            let pid = unsafe { nix::libc::fork() };
-
-            match pid {
-                0 => {self.main_handler.as_ref().run()}
-                _ => {main_childs.push(pid)}
+            let mut manager = WorkerManager::new(group, None);
+            let _ = manager.start();
+            loop {
+                manager.manage();
             }
         }
-
-        for _ in 0..self.config.reserve {
-            let pid = unsafe {nix::libc::fork()};
-        }
-
-
-        return Ok(0)
     }
 }
