032cecfe82b69d6734ab365926725d7b6686e6cb
ksmail13 <ksmail13@gmail.com>
2025-08-31 22:41:15 +0900

    divide worker manager
    - add workergroup
    - add workerclenaer

--- src/worker.rs
+++ src/worker.rs
@@ -1,13 +1,14 @@
-use std::{process::exit, rc::Rc, vec};
+use std::{process::exit, rc::Rc};
 
 use nix::{
+    Error,
     errno::Errno,
     sys::{
         signal::{
             Signal::{self},
             kill,
         },
-        wait::{WaitStatus, waitpid},
+        wait::{WaitStatus, wait},
     },
     unistd::{ForkResult, Pid, fork},
 };
@@ -16,20 +17,20 @@ pub trait Worker {
     fn run(&self);
 }
 
-pub struct WorkerManager {
+pub struct WorkerGroup {
     count: u32,
     stopped: bool,
     pids: Vec<Pid>,
     worker: Option<Rc<dyn Worker>>,
 }
 
-impl Drop for WorkerManager {
+impl Drop for WorkerGroup {
     fn drop(&mut self) {
         let _ = self.stop();
     }
 }
 
-impl WorkerManager {
+impl WorkerGroup {
     pub fn new(count: u32, worker: Option<Rc<dyn Worker>>) -> Self {
         let pids = Vec::new();
 
@@ -41,6 +42,45 @@ impl WorkerManager {
         };
 
         return new_struct;
+    }
+
+    fn start(&mut self) -> Result<(), &str> {
+        let mut remains = self.count;
+        let threshold = 5;
+        for _ in 0..threshold {
+            for _ in 0..remains {
+                if let Ok(pid) = self.fork_child() {
+                    self.pids.push(pid);
+                    remains -= 1;
+                }
+            }
+            if remains == 0 {
+                return Ok(());
+            }
+        }
+        return Err("Failed run workers");
+    }
+
+    fn stop(&mut self) -> Result<(), &str> {
+        if self.stopped {
+            return Ok(());
+        }
+
+        let pids: Vec<Pid> = self.pids.iter().map(|p| p.clone()).collect();
+        for pid in pids {
+            let wait_result = self.kill_child(pid);
+            if let Err(err) = wait_result {
+                if err != Errno::ECHILD && err != Errno::ESRCH {
+                    log::error!(target: "WorkerManager.stop", "child kill & wait failed: {err}");
+                }
+            }
+        }
+        self.stopped = true;
+        return Ok(());
+    }
+
+    fn kill_child(&mut self, pid: Pid) -> Result<(), Errno> {
+        return kill(pid, Some(Signal::SIGINT));
     }
 
     fn fork_child(&mut self) -> Result<Pid, Errno> {
@@ -59,106 +99,114 @@ impl WorkerManager {
                 }
                 exit(0);
             }
-            Err(err) => {
-                log::warn!(target: "WorkerManager.start", "fork failed: {err}");
-                Err(err)
-            }
+            Err(err) => Err(err),
         };
     }
 
-    pub fn start(&mut self) -> Result<(), &str> {
-        let mut remains = self.count;
-        let threshold = 5;
-        for _ in 0..threshold {
-            for _ in 0..remains {
-                if let Ok(pid) = self.fork_child() {
-                    self.pids.push(pid);
-                    remains -= 1;
-                }
-            }
-            if remains == 0 {
-                return Ok(());
-            }
-        }
-        return Err("Failed run workers");
+    pub fn is_this_group(&self, pid: Pid) -> bool {
+        return self.pids.iter().any(|p| p.as_raw() == pid.as_raw());
     }
 
-    pub fn stop(&mut self) -> Result<(), &str> {
-        if self.stopped {
-            return Ok(());
+    pub fn collect_and_fork(&mut self, pid: Pid) -> Result<Pid, Error> {
+        if let Some(idx) = self.pids.iter().position(|p| p.as_raw() == pid.as_raw()) {
+            self.pids.remove(idx);
         }
 
-        let pids: Vec<Pid> = self.pids.iter().map(|p| p.clone()).collect();
-        for pid in pids {
-            let wait_result = self.kill_child(pid);
-            if let Err(err) = wait_result {
-                if err != Errno::ECHILD && err != Errno::ESRCH {
-                    log::error!(target: "WorkerManager.stop", "child kill & wait failed: {err}");
-                }
-            }
-        }
-        self.wait_child();
-        self.stopped = true;
-        return Ok(());
-    }
-
-    fn kill_child(&mut self, pid: Pid) -> Result<(), Errno> {
-        return kill(pid, Some(Signal::SIGINT));
-    }
-
-    fn wait_child(&mut self) -> (usize, Vec<Errno>) {
-        let mut waited_process: Vec<Pid> = vec![];
-        let mut errors: Vec<(Pid, Errno)> = vec![];
-
-        let childs = self.pids.clone();
-
-        for p in childs {
-            let wait_result = waitpid(p, None);
-
-            match wait_result {
-                Ok(WaitStatus::Exited(pid, _)) => waited_process.push(pid),
-                Ok(ws) => log::trace!("{ws:?}"),
-                Err(err) => errors.push((p, err)),
-            }
-        }
-
-        for wp in &waited_process {
-            if let Some(idx) = self.pids.iter().position(|p| p.as_raw() == wp.as_raw()) {
-                self.pids.remove(idx);
-            }
-        }
-
-        let mut ret_err: Vec<Errno> = vec![];
-
-        for (pid, err) in errors {
-            if err == Errno::ECHILD {
-                if let Some(idx) = self.pids.iter().position(|p| p.as_raw() == pid.as_raw()) {
-                    self.pids.remove(idx);
-                }
-            } else {
-                ret_err.push(err);
-            }
-        }
-
-        return (waited_process.len(), ret_err);
-    }
-
-    pub fn manage(&mut self) {
-        let (waited, fails) = self.wait_child();
-        fails
-            .iter()
-            .filter(|e| **e != Errno::EINVAL)
-            .for_each(|e| log::error!("wait failed {e}"));
-        if waited > 0 {
-            log::debug!("wait {waited} processes");
-        }
-        for _ in 0..waited {
-            let _ = self.fork_child();
-        }
+        return self.fork_child();
     }
 
     pub fn set_worker(&mut self, worker: Option<Rc<dyn Worker>>) {
         self.worker = worker;
+    }
+}
+
+pub enum WaitError {
+    ErrorExit(Pid, i32),
+    WaitFailed(Errno),
+    NotExited(WaitStatus),
+}
+
+impl std::fmt::Display for WaitError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return match self {
+            WaitError::ErrorExit(pid, exit_code) => {
+                f.write_fmt(format_args!("ErrorExit({pid}, {exit_code})"))
+            }
+            WaitError::WaitFailed(errno) => f.write_fmt(format_args!("WaitFailed({errno})")),
+            WaitError::NotExited(wait_status) => {
+                f.write_fmt(format_args!("NotExited({wait_status:?})"))
+            }
+        };
+    }
+}
+
+pub struct WorkerCleaner;
+
+impl WorkerCleaner {
+    pub fn wait(&self) -> Result<Pid, WaitError> {
+        let wait_result = wait();
+        return match wait_result {
+            Ok(WaitStatus::Exited(pid, excode)) => {
+                if excode == 0 {
+                    Ok(pid)
+                } else {
+                    Err(WaitError::ErrorExit(pid, excode))
+                }
+            }
+            Ok(ws) => Err(WaitError::NotExited(ws)),
+            Err(e) => Err(WaitError::WaitFailed(e)),
+        };
+    }
+}
+
+pub struct WorkerManager<'a> {
+    groups: Vec<&'a mut WorkerGroup>,
+    cleaner: WorkerCleaner,
+}
+
+impl<'a> WorkerManager<'a> {
+    pub fn new(groups: Vec<&'a mut WorkerGroup>, cleaner: Option<WorkerCleaner>) -> Self {
+        return Self {
+            groups: groups,
+            cleaner: if let Some(c) = cleaner {
+                c
+            } else {
+                WorkerCleaner {}
+            },
+        };
+    }
+
+    pub fn start(&mut self) -> Result<(), &str> {
+        for g in &mut self.groups {
+            let start_result = g.start();
+            if let Err(err) = start_result {
+                log::error!("start failed: {err}");
+            }
+        }
+        return Ok(());
+    }
+
+    pub fn manage(&mut self) {
+        loop {
+            match self.cleaner.wait() {
+                Ok(pid) => {
+                    for g in &mut self.groups {
+                        if !g.is_this_group(pid) {
+                            continue;
+                        }
+                        let _ = g.collect_and_fork(pid);
+                    }
+                }
+                Err(WaitError::WaitFailed(e)) => {
+                    if e != Errno::ECHILD {
+                        log::error!(target: "WorkerManager.manage", "wait failed {e}");
+                    }
+                }
+                Err(e) => {
+                    log::error!(target: "WorkerManager.manage", "wait error {e}");
+                }
+            }
+        }
     }
 }
 
@@ -199,10 +247,11 @@ mod test {
             .format_line_number(true)
             .write_style(env_logger::fmt::WriteStyle::Always)
             .init();
-        let mut manager = WorkerManager::new(5, Some(Rc::new(SleepWorker {})));
+        let mut group = WorkerGroup::new(5, Some(Rc::new(SleepWorker {})));
+        let mut manager = WorkerManager::new(vec![&mut group], Some(WorkerCleaner {}));
+        let _ = manager.start();
         let pid = getpid();
         log::debug!(target: "test_manager", "start {pid}");
-        let _ = manager.start();
 
         let mut timer = Timer::new(
             ClockId::CLOCK_MONOTONIC,
