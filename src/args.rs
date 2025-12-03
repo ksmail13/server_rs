7e2fb47409b78a7420732b866ad8f47cd2d77c8d
ksmail13 <ksmail13@gmail.com>
2025-12-04 08:32:52 +0900

    remove unused server parameter

--- src/args.rs
+++ src/args.rs
@@ -7,14 +7,10 @@ const DEFAULT_HOST: &str = "0.0.0.0";
 pub struct Args {
     #[arg(short, long, default_value_t = 10080)]
     pub port: u16,
-    #[arg(long, default_value_t = 10079)]
-    pub reserve_port: u16,
     #[arg(long, default_value_t = DEFAULT_HOST.to_string())]
     pub host: String,
     #[arg(short, long, default_value_t = 4)]
     pub worker: u32,
-    #[arg(short, long, default_value_t = 0)]
-    pub reserve: u32,
     #[arg(short, long, default_value_t = 500)]
     pub timeout_ms: u64,
 }
